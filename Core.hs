7bc77efd8b49c477bdb10ade0d7ec21e02f2650f
ingBE <39978525+ingBE@users.noreply.github.com>
2018-07-07 02:35:21 +0900

    Add files via upload

--- Core.hs
+++ Core.hs
@@ -1,5 +1,19 @@
 module Core where
 
+newtype Vector a = Vector (a,a,a) deriving (Eq,Show)
+instance Num a => Num (Vector a) where
+    Vector (x1,y1,z1) + Vector (x2,y2,z2) = Vector (x1+x2,y1+y2,z1+z2)
+    Vector (x1,y1,z1) - Vector (x2,y2,z2) = Vector (x1-x2,y1-y2,z1-z2)
+    Vector (x1,y1,z1) * Vector (x2,y2,z2) = Vector (y1*z2-y2*z1,z1*x2-z2*x1,x1*y2-x2*y1)
+    abs (Vector (x,y,z)) = Vector (abs x,abs y,abs z)
+    signum (Vector (x,y,z)) = Vector (signum x,signum y,signum z)
+    fromInteger i = Vector (fromInteger i,fromInteger i,fromInteger i)
+
+zeroVector :: Num a => Vector a
+zeroVector = Vector (0,0,0)    
+
+{-
+--using type and function
 type Vector = (Float, Float, Float)
 
 zeroVector :: Vector
@@ -47,3 +61,5 @@ normalize v = v // (magnitude v)
 
 makeOrthonormalBasis :: [Vector] -> [Vector]
 makeOrthonormalBasis [v1,v2,v3] = [normalize v1,normalize (cross v3 v1),cross (normalize v1) (normalize (cross v3 v1))]
+-}
+
--- main.hs
+++ main.hs
@@ -1,5 +1,9 @@
 import Core
-import Particle
+--import Particle
+
+a :: Vector Integer
+a = Vector (1,0,0)
 
 main = do
-    putStrLn.show $ (\[v1,v2,v3] -> v2 !* v3) (makeOrthonormalBasis [(5,0,0),(0,2,3),(2,1,5)])
+    putStrLn.show $ zeroVector
+--    putStrLn.show $ (\[v1,v2,v3] -> v2 !* v3) (makeOrthonormalBasis [(5,0,0),(0,2,3),(2,1,5)])
